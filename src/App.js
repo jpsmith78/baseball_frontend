21c601125464d73f2ea394838a1b11a05ae33b5e
Carlos Godoy <cgod720@gmail.com>
2019-03-03 15:41:05 -0500

    added form and create person route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Header from './components/Header'
+import Form from './components/Form'
 
 class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      currentView: 'persons', //other page is cards
+      currentView: 'people', //other page is cards
       persons: [],
       cards: []
     }
@@ -22,6 +23,27 @@ class App extends Component {
       })
       .catch((err) => {
         console.log(err);
+      })
+  }
+
+  handleCreatePerson = (person) => {
+    fetch('http://localhost:3000/person', {
+      body: JSON.stringify(person),
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((createdPerson) => {
+        return createdPerson.json()
+      })
+      .then((jData) => {
+        this.updateArray(jData, 'persons')
+        this.handleView('people')
+      })
+      .catch((err) => {
+          console.log(err);
       })
   }
 
@@ -57,6 +79,15 @@ class App extends Component {
     })
   }
 
+  updateArray = (person, array) => {
+    this.setState((prevState) => {
+      prevState[array].push(person)
+      return {
+        [array]: prevState[array]
+      }
+    })
+  }
+
   componentDidMount() {
     this.fetchPersons()
     this.fetchCards()
@@ -67,6 +98,7 @@ class App extends Component {
     return (
       <div className="main-page">
         <Header />
+        <Form handleCreatePerson={this.handleCreatePerson}/>
       </div>
     );
   }
