40680e22b610ee45ded188a0a9b309f404f63e78
Justin Smith <justinsmith@Justins-MacBook-Pro.local>
2019-03-04 13:29:12 -0500

    corrected image alt in person.js

--- src/components/Person.js
+++ src/components/Person.js
@@ -11,18 +11,19 @@ class Person extends Component {
           <li>Age: {this.props.person.age}</li>
           <li>Interest: {this.props.person.interest}</li>
           <li>Collection:
-                {this.props.person.collection.map((player, index)=>{
+            { this.props.person.collection ?
+                this.props.person.collection.map((player, index)=>{
                   return(
                     <ul key={index}>
                       <li >{player.player}</li>
                       <li >{player.team}</li>
-                      <li ><img src={player.image} alt="player" /></li>
+                      <li ><img src={player.image} alt={player.player} /></li>
                       <li >{player.position}</li>
                       <li > {player.batting_avg}</li>
                       <br />
                     </ul>
                   )
-                })}
+                }) : "" }
           </li>
         </ul>
       </div>
