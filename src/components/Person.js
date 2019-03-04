ae63bc126cc8d0deefa42c48c192ade672ba57fa
Justin Smith <justinsmith@Justins-MacBook-Pro.local>
2019-03-04 15:33:18 -0500

    added collection to people

--- src/components/Person.js
+++ src/components/Person.js
@@ -6,11 +6,29 @@ class Person extends Component {
   render(){
     return(
       <div className="person-container">
+        <ul>
+          <li>User: {this.props.person.name}</li>
+          <li>Age: {this.props.person.age}</li>
+          <li>Interest: {this.props.person.interest}</li>
+          <li>Collection:
 
-          User: {this.props.person.name}<br/>
-          Age: {this.props.person.age}<br/>
-          Interest: {this.props.person.interest}<br/>
-        <br/>
+              { this.props.person.collection ?
+                this.props.person.collection.map((player, index)=>{
+                return(
+                  <ul key={index}>
+                  <li>{player.player}</li>
+                  <li>{player.team}</li>
+                  <li><img src={player.image} alt={player.player}/></li>
+                  <li>{player.position}</li>
+                  <li>{player.batting_avg}</li>
+                  <br />
+                  </ul>
+
+                )
+              }) : ""}
+          </li>
+
+        </ul>
       </div>
     )
   }
